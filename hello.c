df933ff84643ca08d59f5adfec7960c5a9024cfd
Marcus Mendoza <115035701+marcusmdza@users.noreply.github.com>
2024-10-01 18:40:46 +0000

    Cat Art

--- hello.c
+++ hello.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 
 int main() {
-  char hello[] = "Hello this is a new message!"; 
+  char hello[] = "Hello this is a new message!";
+  char cat[] = 
+  " |\---/|
+    | ,_, |
+      \_`_/-..----.
+  ___/ `   ' ,""+ \  sk
+  (__...'   __\    |`.___.';
+    (_,...'(_,.`__)/'.....+" 
   char hello2[] = { 72, 101, 108, 108, 111, 33, 0 };
 
   puts(hello);
   puts(hello2);
+  puts(cat);
 
   return 0;
 }
